693302361b56a47636c04b4e0cdc76f6448e811c
Jai <malhi.jaivir@gmail.com>
2023-05-24 15:50:48 -0700

    created a1 repo locally

---
+++ src/test/java/a1/histogram/assign1/Assign1ApplicationTests.java
@@ -0,0 +1,13 @@
+package a1.histogram.assign1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assign1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
